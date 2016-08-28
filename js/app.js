9407ee84781092ac14183bc52143034baf4c9d4e
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2016-08-28 00:01:39 -0400

    init new website

--- js/app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("ocTranspoFeedback", []);
-app.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
